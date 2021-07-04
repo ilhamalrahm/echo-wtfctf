248aa45a30b9d6517abc69af4ccd36690caaf843
Parthiv-M <parthiv.menon@learner.manipal.edu>
2021-07-04 16:02:54 +0530

    removed debug line

--- index.js
+++ index.js
@@ -5,7 +5,6 @@ const counter = (from, to, element, duration) => {
     const step = (time) => {
         if(!startTime) startTime = time;
         const progress = Math.min((time - startTime) / duration, 1);
-        console.log(Math.floor(progress * (to - from) + from))
         element.innerHTML = Math.floor(progress * (to - from) + from);
         if (progress < 1) {
             window.requestAnimationFrame(step);
