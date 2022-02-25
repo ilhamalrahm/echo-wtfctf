948fdb6d59adf0b4524a209068e1f058597f06ef
Parthiv-M <parthiv.menon@learner.manipal.edu>
2022-02-26 01:00:16 +0530

    added event countdown timer

--- index.js
+++ index.js
@@ -18,3 +18,21 @@ counter(1300, 1392, document.getElementById("solves"), 2000);
 counter(900, 993, document.getElementById("users"), 1800);
 counter(700, 789, document.getElementById("teams"), 1500);
 
+// animate the countdown
+const countdownFunction = () => {
+    const second = 1000,
+          minute = second * 60,
+          hour = minute * 60,
+          day = hour * 24;
+    const countDown = new Date("02/27/2022").getTime();
+    x = setInterval(function() {    
+      const now = new Date().getTime(),
+            distance = countDown - now;
+        document.getElementById("days").innerText = Math.floor(distance / (day)),
+        document.getElementById("hours").innerText = Math.floor((distance % (day)) / (hour)),
+        document.getElementById("minutes").innerText = Math.floor((distance % (hour)) / (minute)),
+        document.getElementById("seconds").innerText = Math.floor((distance % (minute)) / second);
+    }, 0)
+}
+
+countdownFunction()
